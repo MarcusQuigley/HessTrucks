68197d74cb4dede7fc5fce9083f822df4bf67c6c
Marcus Quigley <marcusquigley@gmail.com>
2021-02-10 11:56:50 -0500

    init commit

---
+++ src/Services/Catalog/Services.Catalog.Api/Controllers/TruckController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.Api.Controllers
+{
+    [Route("api/trucks")]
+    [ApiController]
+    public class TruckController : ControllerBase
+    {
+        [HttpGet]
+        public Task<string> Greeting()
+        {
+            //return new Task<string>(()=> "Hello world");
+
+         return   Task.FromResult("Hello world");
+        }
+    }
+}
