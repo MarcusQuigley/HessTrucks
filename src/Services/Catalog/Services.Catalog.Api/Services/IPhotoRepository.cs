c5dcb4e1cc9ff2a95ddfd2061ad13f173f2ff5e6
Marcus Quigley <marcusquigley@gmail.com>
2021-02-12 11:03:52 -0500

    Adding Category/Photo repos to Catalog Api

---
+++ src/Services/Catalog/Services.Catalog.Api/Services/IPhotoRepository.cs
@@ -0,0 +1,16 @@
+﻿using Services.Catalog.Api.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Services.Catalog.Api.Services
+{
+  public  interface IPhotoRepository
+    {
+        Task<IEnumerable<Photo>> GetPhotosByTruckId(Guid truckId);
+        Task AddPhoto(Photo photo);
+        Task UpdatePhoto(Photo photo);
+        Task<bool> SaveChanges();
+    }
+}
